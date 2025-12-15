3c34dccc723912a6d0ad8b5655a930ea8f106e48
Maria Villagra <102599157+MariaVillagra2309@users.noreply.github.com>
2025-12-15 01:24:22 -0300

    Agregua funcionalidad en InformeAsistencias.aspx.cs

--- Vistas/InformeAsistencias.aspx.cs
+++ Vistas/InformeAsistencias.aspx.cs
@@ -9,9 +9,47 @@ namespace Vistas
 {
     public partial class InformeAsistencias : System.Web.UI.Page
     {
+        ClinicaUsuario clinica = new ClinicaUsuario();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+             lblUsuario.Text = Session["Nombre"].ToString();
         }
+
+        protected void btnGenerar_Click(object sender, EventArgs e)
+        {
+            DateTime desde = Convert.ToDateTime(txtDesde.Text);
+            DateTime hasta = Convert.ToDateTime(txtHasta.Text);
+
+            DataTable tabla = clinica.ObtenerInformeAsistencias(desde, hasta);
+            gvAsistencias.DataSource = tabla;
+            gvAsistencias.DataBind();
+            CalcularResumen(tabla);
+        }
+
+       private void CalcularResumen(DataTable tabla)
+       {
+         int total = tabla.Rows.Count;
+         if (total == 0)
+         {
+             lblPorcentajes.Text = "No hay datos para el rango de fecha seleccionado.";
+             return;
+         }
+         int presentes = 0;
+         int ausentes = 0;
+
+         foreach (DataRow turno in tabla.Rows)
+         {
+             int estado = Convert.ToInt32(turno["Estado_Turno"]);
+
+             if (estado == 1)
+                 presentes++;
+             else if (estado == 2)
+                 ausentes++;
+
+            int pAtendidos = presentes * 100 / total;
+            int pAusentes = ausentes * 100 / total;
+            lblPorcentajes.Text = $"{pAtendidos}% Presentes " + $" {pAusentes}%  Ausentes";
+       }
+       }
     }
 }
